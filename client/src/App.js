b3e027a3d672d185b40a5e9c60cbb623bbb913d9
bismansahni <bismansahni@outlook.com>
2024-06-10 17:17:11 +0530

    updated the dashboard name2

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Login />} />
                 <Route path="/register" element={<Register />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={<DashBoard />} />
                 <Route path="/account-settings" element={<AccountSettings />} />
             </Routes>
         </Router>
