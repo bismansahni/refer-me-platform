93e80186a8e838a35d874aae57d132108c3e8523
bismansahni <bismansahni@outlook.com>
2024-06-13 03:31:43 +0530

    implementing the notifications

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -1,8 +1,8 @@
 // src/components/Notifications.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Navbar from './Navbar'; // Ensure you have a Navbar component
-import './styles/Notifications.css'; // Ensure you have appropriate styles
+import Navbar from './Navbar';
+import './styles/Notifications.css';
 
 const apiUrl = process.env.REACT_APP_API_BASE_URL;
 
@@ -12,18 +12,38 @@ const Notifications = () => {
     useEffect(() => {
         const fetchNotifications = async () => {
             const token = localStorage.getItem('token');
+            console.log('Token:', token); // Log the token
+
+            if (!token) {
+                console.error('No token found in localStorage');
+                return;
+            }
+
+            const endpoint = `${apiUrl}/api/notifications`;
+            console.log('Fetching notifications from:', endpoint); // Log the API endpoint
+
             try {
-                const res = await axios.get(`${apiUrl}/api/notifications`, {
+                const res = await axios.get(endpoint, {
                     headers: {
                         'x-auth-token': token
                     }
                 });
+                console.log('Notifications received:', res.data); // Debug log for received notifications
                 setNotifications(res.data);
+                localStorage.setItem('notifications', JSON.stringify(res.data)); // Store notifications in localStorage
             } catch (error) {
                 console.error('Error fetching notifications', error);
             }
         };
-        fetchNotifications();
+
+        const storedNotifications = localStorage.getItem('notifications');
+        if (storedNotifications) {
+            console.log('Using stored notifications from localStorage'); // Log when using localStorage
+            setNotifications(JSON.parse(storedNotifications));
+        } else {
+            console.log('No stored notifications found, fetching from server'); // Log when fetching from server
+            fetchNotifications();
+        }
     }, []);
 
     return (
